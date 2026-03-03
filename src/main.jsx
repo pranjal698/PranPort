0e7578888eea12bf273c18813e0084d7f3fe3754
Pranjal <pranjal698@users.noreply.github.com>
2026-03-04 01:40:35 +0530

    Fix Pages build pipeline and remove conflicting workflow

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import Lenis from "@studio-freight/lenis";
-
-const lenis = new Lenis({ smooth: true, lerp: 0.08 });
-
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
 
 ReactDOM.createRoot(document.getElementById("root"))
   .render(<App />);
